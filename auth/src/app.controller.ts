3c230c664d58d9674b7387f514ff8cc849399d90
pedrokohler <pedrokohlerbh@gmail.com>
2021-08-11 19:12:28 -0300

    Implement sign up validation

--- auth/src/app.controller.ts
+++ auth/src/app.controller.ts
@@ -1,11 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { UserSignUpDto } from './dtos/user-sign-up.dto';
 
-@Controller()
+@Controller('api/users')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('api/users/current-user')
+  @Post('/sign-in')
+  signIn(): string {
+    return this.appService.getHello();
+  }
+
+  @Post('/sign-out')
+  signOut(): string {
+    return this.appService.getHello();
+  }
+
+  @Post('/sign-up')
+  signUp(@Body() userSignUpDto: UserSignUpDto): string {
+    console.log("Creating user")
+    return this.appService.getHello();
+  }
+
+  @Get('/current-user')
   getHello(): string {
     return this.appService.getHello();
   }
