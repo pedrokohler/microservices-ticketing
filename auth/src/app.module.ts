7a1c67e866919fc5d363058c6e17b7c1eda2af4e
pedrokohler <pedrokohlerbh@gmail.com>
2021-08-11 20:28:15 -0300

    Add mongoose to auth service

--- auth/src/app.module.ts
+++ auth/src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module, ValidationPipe } from '@nestjs/common';
 import { APP_PIPE } from '@nestjs/core';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://auth-mongo-cluster-ip:27017/auth', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [
     AppService,
