75c88e5e0ff1a6a45f02653280f9a395ac3db4e3
pedrokohler <pedrokohlerbh@gmail.com>
2021-08-12 13:45:35 -0300

    Add cookie session to auth service

--- auth/src/main.ts
+++ auth/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import cookieSession from 'cookie-session';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    cookieSession({
+      signed: false,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
