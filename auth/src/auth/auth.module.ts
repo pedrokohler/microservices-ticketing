e37a22e2a1bea2b09d6c2773bda89b99de39ec28
Pedro Kohler <pedrokohlerbh@gmail.com>
2021-08-12 20:59:47 -0300

    auth: import user and password modules

--- auth/src/auth/auth.module.ts
+++ auth/src/auth/auth.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PasswordModule } from 'src/password/password.module';
+import { UserModule } from 'src/user/user.module';
 import { AuthService } from './auth.service';
 
 @Module({
-  providers: [AuthService]
+  imports: [UserModule, PasswordModule],
+  providers: [AuthService],
 })
 export class AuthModule {}
