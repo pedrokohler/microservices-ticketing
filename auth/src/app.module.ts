5cf2b05cffa649986cde1f6c07d7742d71889fe6
Pedro Kohler <pedrokohlerbh@gmail.com>
2021-08-12 20:28:57 -0300

    Scaffold auth module and service

--- auth/src/app.module.ts
+++ auth/src/app.module.ts
@@ -5,6 +5,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PasswordModule } from './password/password.module';
 import { UserProvider } from './schemas/user.provider';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import { UserProvider } from './schemas/user.provider';
       useCreateIndex: true,
     }),
     MongooseModule.forFeatureAsync([UserProvider]),
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [
