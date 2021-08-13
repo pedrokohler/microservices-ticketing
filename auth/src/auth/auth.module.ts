6ce5e99e0297e6cc325b3e4a9c35883ed44a088e
Pedro Kohler <pedrokohlerbh@gmail.com>
2021-08-12 21:27:38 -0300

    Add auth controller

--- auth/src/auth/auth.module.ts
+++ auth/src/auth/auth.module.ts
@@ -4,7 +4,6 @@ import { UserModule } from 'src/user/user.module';
 import { AuthService } from './auth.service';
 
 @Module({
-  imports: [UserModule, PasswordModule],
-  providers: [AuthService],
+  controllers: [AuthController],
 })
 export class AuthModule {}
