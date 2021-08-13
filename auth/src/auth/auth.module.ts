de36155f1f4e7bf18c22bd009ebbb53a46f82241
Pedro Kohler <pedrokohlerbh@gmail.com>
2021-08-12 21:32:28 -0300

    Revert "Add auth controller"
    This reverts commit 6ce5e99e0297e6cc325b3e4a9c35883ed44a088e.

--- auth/src/auth/auth.module.ts
+++ auth/src/auth/auth.module.ts
@@ -4,11 +4,9 @@ import { PasswordModule } from 'src/password/password.module';
 import { UserModule } from 'src/user/user.module';
 import { AuthService } from './auth.service';
 import { LocalStrategy } from './strategies/local.strategy';
-import { AuthController } from './auth.controller';
 
 @Module({
   imports: [UserModule, PasswordModule, PassportModule],
   providers: [AuthService, LocalStrategy],
-  controllers: [AuthController],
 })
 export class AuthModule {}
