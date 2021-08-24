7a137d50c8caf36ba6f257406dd5fe9e04ec5dc2
Pedro Kohler <pedrokohlerbh@gmail.com>
2021-08-24 20:55:56 -0300

    Add jwt secret to kubernetes

--- auth/src/main.ts
+++ auth/src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 import cookieSession from 'cookie-session';
 
 async function bootstrap() {
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY must be defined');
+  }
+
   const app = await NestFactory.create(AppModule);
   app.use(
     cookieSession({
